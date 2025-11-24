6a64f75ff4b380cd6d4bcb06a4d5c522539d6196
NELSON GOMES HAMMEL <nelson.hammel@ibratec.com.br>
2025-11-24 09:28:38 -0300

    Teste 1 - PROC04

---
+++ R__PROC04.sql
@@ -0,0 +1,20 @@
+USE [PROD]
+GO
+
+/****** Object:  StoredProcedure [dbo].[Usr_sp_integranotasfiscais]    Script Date: 29/10/2025 15:38:18 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+
+
+
+
+
+CREATE OR REPLACE PROCEDURE [dbo].[PROC04]
+AS
+SELECT 1+5 AS CONT
+
+GO
