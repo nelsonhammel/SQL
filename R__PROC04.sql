fe1e27c30c1c52da24a4c73a9e6597c294af20a1
NELSON GOMES HAMMEL <nelson.hammel@ibratec.com.br>
2025-11-24 11:02:18 -0300

    teste 10

--- R__PROC04.sql
+++ R__PROC04.sql
@@ -1,6 +1,7 @@
 USE [PROD]
 GO
 
+/******Versao do Arquivo: ($Id$)******/
 /****** Object:  StoredProcedure [dbo].[Usr_sp_integranotasfiscais]    Script Date: 29/10/2025 15:38:18 ******/
 SET ANSI_NULLS ON
 GO
@@ -15,8 +16,6 @@ GO
 
 ALTER  PROCEDURE [dbo].[PROC04]
 AS
-SELECT 3+1 AS CONT
+SELECT 8+5 AS CONT
 
 GO
-
--- Versão do Git: ${gitCommit}
