92c3b85f0d9f1aa94d1af81f5d294f1e5fcbdfa4
NELSON GOMES HAMMEL <nelson.hammel@ibratec.com.br>
2025-11-24 15:45:35 -0300

    teste proc 5

---
+++ R__PROC05.sql
@@ -0,0 +1,21 @@
+USE [PROD]
+GO
+
+-- Hash do Arquivo: $Id$
+/****** Object:  StoredProcedure [dbo].[Usr_sp_integranotasfiscais]    Script Date: 29/10/2025 15:38:18 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+
+
+
+
+
+CREATE  PROCEDURE [dbo].[PROC05]
+AS
+SELECT 5+5 AS CONT
+
+GO
