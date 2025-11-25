bc3dfdc23d1618c14a3a58eaf861ca2c3ef9bf9f
nelsonhammel <nelson.hammel@ibratec.com.br>
2025-11-25 09:12:34 -0300

    Create R__PROC06.sql

---
+++ R__PROC06.sql
@@ -0,0 +1,21 @@
+USE [PROD]
+GO
+
+
+/****** Object:  StoredProcedure [dbo].[Usr_sp_integranotasfiscais]    Script Date: 29/10/2025 15:38:18 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+
+
+
+
+
+CREATE  PROCEDURE [dbo].[PROC06]
+AS
+SELECT 6+6 AS CONT
+
+GO
